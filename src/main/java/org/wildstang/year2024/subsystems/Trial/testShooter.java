9d5de1f63f13426d2bfd072c31d34f4820b76246
Alex Martinez <112990313+Blublub001@users.noreply.github.com>
2024-03-06 18:54:57 -0600

    Blue Wing / Blue Blitz A Paths
    1st attempt at autos

---
+++ src/main/java/org/wildstang/year2024/subsystems/Trial/testShooter.java
@@ -0,0 +1,50 @@
+package org.wildstang.year2024.subsystems.Trial;
+
+import org.wildstang.framework.core.Core;
+import org.wildstang.framework.io.inputs.Input;
+import org.wildstang.framework.subsystems.Subsystem;
+import org.wildstang.hardware.roborio.outputs.WsSpark;
+import org.wildstang.year2024.robot.WsOutputs;
+import org.wildstang.year2024.robot.WsInputs;
+
+
+public class testShooter implements Subsystem{
+
+    @Override
+    public void inputUpdate(Input source) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'inputUpdate'");
+    }
+
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'init'");
+    }
+
+    @Override
+    public void selfTest() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'selfTest'");
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void resetState() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resetState'");
+    }
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    
+}
