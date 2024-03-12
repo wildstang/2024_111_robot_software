73fa6af53cd4dcd9a3ed74aa4c2fd30e2570c725
Stuart McCrorie <42500981+smccrorie@users.noreply.github.com>
2024-03-11 19:59:40 -0500

    Auto updates and cycle feeding

--- src/main/java/org/wildstang/year2024/auto/Programs/Offsides.java
+++ src/main/java/org/wildstang/year2024/auto/Programs/Offsides.java
@@ -41,7 +41,7 @@ public class Offsides extends AutoProgram {
         addStep(new ShooterSetAngle(175));
         addStep(new SetFlywheel(true));
         addStep(new VisionOnStep(true));
-        addStep(new AutoStepDelay(500));
+        addStep(new AutoStepDelay(300));
 
         // shoot preload
         addStep(new ShootSpeakerStep());
@@ -50,9 +50,9 @@ public class Offsides extends AutoProgram {
         AutoParallelStepGroup group1 = new AutoParallelStepGroup();
         group1.addStep(new SwervePathFollowerStep("GrabE Offside", swerve, isBlue));
         AutoSerialStepGroup group1a = new AutoSerialStepGroup();
-        group1a.addStep(new SetIntakeSequenceStep(true));
-        group1a.addStep(new ShooterSetAngle(130));
         group1a.addStep(new AutoStepDelay(300));
+        group1a.addStep(new SetIntakeSequenceStep(true));
+        group1a.addStep(new ShooterSetAngle(70));
         group1a.addStep(new ShooterAutoAim(true));
         group1a.addStep(new VisionOnStep(false));
         group1.addStep(group1a);
@@ -67,6 +67,7 @@ public class Offsides extends AutoProgram {
         group2.addStep(group2a);
         addStep(group2);
         addStep(new ShootSpeakerStep());
+        addStep(new AutoStepDelay(300));
         addStep(new SetIntakeSequenceStep(true));
         addStep(new VisionOnStep(false));
 
@@ -82,8 +83,19 @@ public class Offsides extends AutoProgram {
         group4.addStep(group4a);
         addStep(group4);
         addStep(new ShootSpeakerStep());
+        addStep(new AutoStepDelay(300));
+        addStep(new SetIntakeSequenceStep(true));
+        addStep(new VisionOnStep(false));
 
         addStep(new SwervePathFollowerStep("GrabC Offside", swerve, isBlue));
+        AutoParallelStepGroup group5 = new AutoParallelStepGroup();
+        group5.addStep(new SwervePathFollowerStep("ScoreC Offside", swerve, isBlue));
+        AutoSerialStepGroup group5a = new AutoSerialStepGroup();
+        group5a.addStep(new AutoStepDelay(1000));
+        group5a.addStep(new VisionOnStep(true));
+        group5.addStep(group5a);
+        addStep(group5);
+        addStep(new ShootSpeakerStep());
 
     }
 
